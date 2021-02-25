d1cefac2233a4a8cf003614a6671f12dab59c501
MariaMunozR <maria.munoz.rubio@gmail.com>
2021-02-25 11:21:23 +0100

    add prod env & prod js

---
+++ config/webpack/prod.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const base = require('./base');
+const helpers = require ('./helpers')
+
+module.exports = merge(base, {
+  mode: 'production',
+  output: {
+      path:helpers.resolveFromRootPath('dist'),
+      filename: './js/[name].[chunkhash].js' 
+  },
+
+module:{
+    rules:[
+        {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+            name: './images/[name].[hash].[ext]',
+        },
+    },
+    ],
+},
+});
